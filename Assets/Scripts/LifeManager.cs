58b4fa90d98f5b283122573ba951f6e210b33252
Ro8s <rodrigo.delgado3132@gmail.com>
2022-08-30 20:17:07 -0300

    added health canvas and logic of enemy damage

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// LifeManager class that defines the health to the player
+/// </summary>
 public class LifeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// L character
+    /// </summary>
+    [SerializeField] GameObject L_chr;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// I character
+    /// </summary>
+    [SerializeField] GameObject I_chr;
+
+    /// <summary>
+    /// F character
+    /// </summary>
+    [SerializeField] GameObject F_chr;
+
+    /// <summary>
+    /// E character
+    /// </summary>
+    [SerializeField] GameObject E_chr;
+
+    ///PlayerCollider to detect the damage to the player
+    ///[SerializeField] Collider playerCollider;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Enemy")
+        {
+            if (E_chr.activeInHierarchy == true)
+                E_chr.SetActive(false);
+            else if (F_chr.activeInHierarchy == true)
+                F_chr.SetActive(false);
+            else if (I_chr.activeInHierarchy == true)
+                I_chr.SetActive(false);
+            else if (F_chr.activeInHierarchy == true)
+                F_chr.SetActive(false);
+            else
+                SceneManager.LoadScene(0);
+        }
     }
 }
