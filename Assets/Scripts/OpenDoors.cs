7c035c860ca0bc4f4e93d6491b565ebb7443569a
Alrick Deperiers <77963722+hollaze@users.noreply.github.com>
2022-08-31 21:40:02 +0200

    [Add]
    Golden particles out of the chest

---
+++ Assets/Scripts/OpenDoors.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoors : MonoBehaviour
+{
+	#region Doors GameObjects Variables
+	[SerializeField] private GameObject[] _waterRoomsDoors;
+	[SerializeField] private GameObject[] _earthRoomsDoors;
+	[SerializeField] private GameObject[] _fireRoomsDoors;
+	#endregion
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
