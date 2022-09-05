ecc3113f964189aef3b5424c5a58d7b59e74d748
Ro8s <rodrigo.delgado3132@gmail.com>
2022-09-05 15:58:37 -0300

    fixing light error and scale sword

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -10,25 +10,11 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class LifeManager : MonoBehaviour
 {
-    /// <summary>
-    /// L character
-    /// </summary>
-    [SerializeField] GameObject L_chr;
 
     /// <summary>
-    /// I character
+    /// Array that had all the characters of LIFE
     /// </summary>
-    [SerializeField] GameObject I_chr;
-
-    /// <summary>
-    /// F character
-    /// </summary>
-    [SerializeField] GameObject F_chr;
-
-    /// <summary>
-    /// E character
-    /// </summary>
-    [SerializeField] GameObject E_chr;
+    [SerializeField] GameObject[] Life;
 
     ///PlayerCollider to detect the damage to the player
     ///[SerializeField] Collider playerCollider;
@@ -37,14 +23,14 @@ public class LifeManager : MonoBehaviour
     {
         if(other.tag == "Enemy")
         {
-            if (E_chr.activeInHierarchy == true)
-                E_chr.SetActive(false);
-            else if (F_chr.activeInHierarchy == true)
-                F_chr.SetActive(false);
-            else if (I_chr.activeInHierarchy == true)
-                I_chr.SetActive(false);
-            else if (F_chr.activeInHierarchy == true)
-                F_chr.SetActive(false);
+            if (Life[3].activeInHierarchy == true)
+                Life[3].SetActive(false);
+            else if (Life[2].activeInHierarchy == true)
+                Life[2].SetActive(false);
+            else if (Life[1].activeInHierarchy == true)
+                Life[1].SetActive(false);
+            else if (Life[0].activeInHierarchy == true)
+                Life[0].SetActive(false);
             else
                 SceneManager.LoadScene(0);
         }
